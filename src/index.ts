1709179d30231ddc0128f21bec0089085ef14566
Korzher <tchuprinin.constantin@yandex.ru>
2025-01-18 19:08:37 +0300

    arrays and tuples syntax

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,10 @@ if (age < 50) {
   age += 10;
 }
 console.log(age);
+
+let numbers: number[] = [1, 2, 3]; //Arrays
+numbers.forEach((n) => n.toFixed());
+
+let user: [number, string] = [1, "Mosh"]; //Tuple
+user[0].toLocaleString();
+user[1].charAt(0);
